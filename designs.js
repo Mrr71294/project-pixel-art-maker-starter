ea38abcb86eadc6042d8b207725d851c4c41b135
Michael Rendon <mrr71294@gmail.com>
2018-04-11 18:09:48 -0400

    Created pixel art app functions

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
-// Select color input
 // Select size input
+const inputColor = $('#colorPicker');
+console.log(inputColor.val());
+// Select size input
+const inputHeight = $('#inputHeight');
+const inputWidth  = $('#inputWidth');
+console.log(inputHeight.val());
+console.log(inputWidth.val());
+
+//Make grid event listener
+$('#submit').on('click',function(event){
+  event.preventDefault();
+  let height = $('#inputHeight').val();
+  let width = $('#inputWidth').val();
+  $('#pixelCanvas').empty();
+  makeGrid(height,width);
+});
+
+//Change color event listner
+$('#pixelCanvas').on('click','td',function(){
+  let color = inputColor.val();
+  console.log(color);
+  $(this).css('background-color',color);
+});
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(height,width) {
+  console.log(height);
+  console.log(width);
 
-function makeGrid() {
-
-// Your code goes here!
-
+  for(i = 0; i < height; i++){
+    $('#pixelCanvas').append($("<tr></tr>"));
+    for(x = 0; x < width; x++){
+      $('tr').last().append($("<td></td>"));
+    }
+  }
 }
